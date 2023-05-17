3f0d1d26c1e9e8fa15e730d6614f6e6f098c44ef
Nathan Cutaran <127146499+in8xx@users.noreply.github.com>
2023-05-16 21:38:12 -0700

    5/16 - Finished jumper, going to submit this one

--- settings.py
+++ settings.py
@@ -2,10 +2,11 @@ WIDTH = 700
 HEIGHT = 700
 PLAYER_ACC = 2
 PLAYER_FRICTION = -0.3
-PLAYER_JUMP = 15
+PLAYER_JUMP = 20
 PLAYER_GRAV = 0.8
-GAME_FONT = 'inkfree'
-FPS = 60
+MOB_ACC = 2
+MOB_FRICTION = -0.3
+GAME_FONT = 'arial'
 
 # define colors
 BLACK = (0,0,0)
@@ -14,12 +15,20 @@ BLUE = (50,50,255)
 BABYBLUE = (137, 207, 240)
 SLIME = (50, 205, 50)
 RED = (255,50,50)
-GOLD = (255, 215, 0)
 
 # defines a random color
 from random import randint
 RANDCOLOR = [randint(0,255), randint(0,255), randint(0,255)]
+RANDCOLOR2 = [randint(0,255), randint(0,255), randint(0,255)]
+
+FPS = 60
+RUNNING = True
+SCORE = 0
+PAUSED = False
 
 # Starting platforms
-PLATFORMS_LIST = (WIDTH / 2 -50, HEIGHT/2, 100, 20, (BABYBLUE), "normal"),
-                
+PLATFORMS_LIST = [(0, HEIGHT - 40, WIDTH, 40, (200,200,200), "normal"),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, (SLIME), "bouncey"),
+                #  (500, HEIGHT - 300, 100, 5, (200,200,200), "disappearing "),
+                 (350, 200, 100, 20, (BABYBLUE), "normal"),
+                 (175, 400, 100, 20, (BABYBLUE), "normal")]
--- sprites.py
+++ sprites.py
@@ -6,7 +6,7 @@ from random import randint
 vec = pg.math.Vector2
 
 # creates a player class
-class Bird(Sprite):
+class Player(Sprite):
     def __init__(self, game):
         pg.sprite.Sprite.__init__(self)
         # properties
@@ -22,15 +22,12 @@ class Bird(Sprite):
         self.canjump = False
 
 # method in player class that defines the inputs and what they do, when this key gets pressed the player acceleration does this...
-    # def input(self):
-    #     keystate = pg.key.get_pressed()
-        
-    #     if keystate[pg.K_a]:
-    #         self.acc.x = -PLAYER_ACC
-      
-    #     if keystate[pg.K_d]:
-    #         self.acc.x = PLAYER_ACC
-
+    def input(self):
+        keystate = pg.key.get_pressed()
+        if keystate[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keystate[pg.K_d]:
+            self.acc.x = PLAYER_ACC
 
 # method in player class that contricts the player jump to the platform 
     def jump(self):
@@ -42,10 +39,10 @@ class Bird(Sprite):
 
 # method in player class that defines the boundaries, in this case it wraps around
     def inbounds(self):
-        if self.rect.x <= 0:
+        if self.rect.x > WIDTH:
            self.pos.x = 0
-        if self.rect.x >= 700:
-            self.pos.x =  650
+        if self.rect.x < 0:
+            self.pos.x = WIDTH
 
 
 
@@ -54,14 +51,53 @@ class Bird(Sprite):
     def update(self):
         self.acc = vec(0, PLAYER_GRAV)
         self.acc.x = self.vel.x * PLAYER_FRICTION
+        self.input()
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
         self.rect.midbottom = self.pos
         self.inbounds()
 
+# creates a mob class, similar to the player
+class Mob(Sprite):
+    def __init__(self,width,height, color):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(randint(1,5),randint(1,5))
+        self.acc = vec(1,1)
+        self.cofric = 0.01
+
+# method that defines the boundaries for the mob sprites
+    def inbounds(self):
+        if self.rect.x > WIDTH:
+            self.vel.x *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.x < 0:
+            self.vel.x *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.y < 0:
+            self.vel.y *= -1
+            # self.acc = self.vel * -self.cofric
+        if self.rect.y > HEIGHT:
+            self.vel.y *= -1
+            # self.acc = self.vel * -self.cofric
+
+# no input as this class is for mobs, however we can modify this to collide witht the boundaries and the player
+    def update(self):
+        self.inbounds()
+        # self.pos.x += self.vel.x
+        # self.pos.y += self.vel.y
+        self.pos += self.vel
+        self.rect.center = self.pos
 
 # creates a new platform class
-class Pipe(Sprite):
+class Platform(Sprite):
     def __init__(self, x, y, width, height, color, variant):
         Sprite.__init__(self)
         self.width = width
@@ -73,3 +109,11 @@ class Pipe(Sprite):
         self.rect.x = x
         self.rect.y = y
         self.variant = variant
+
+class Bouncy(Sprite):
+    def __init__(self, x, y, width, height, color, variant):
+        super().__init__(self, x, y, width, height, color, variant)
+        self.bounce = 15
+
+    def update(self):
+        pass
